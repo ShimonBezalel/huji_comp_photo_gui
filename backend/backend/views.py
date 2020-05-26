b5205cfefa3d4e404c147757a7086815a4dfb900
ehazony <efraimhazony@gmail.com>
2020-05-26 22:52:33 +0300

    image_upload skelliten

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
 
 def test(request):
     return HttpResponse("<html><body>Reached test!</body></html>")
+
+
+
+
+@csrf_exempt
+def upload_images(request):
+    file = request.FILES["images"]
+
+    try:
+        pass
+    # load images to directory
+    except:
+        response = HttpResponse('')
+        response.status_code = 400
+        return response
+    return HttpResponse('')

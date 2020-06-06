dc6c0b0cc838e2201c596dc1dbc22e3d976d5bfc
shimonheimowitz <heimy4prez@gmail.com>
2020-06-06 17:24:54 +0300

    api setup - - viewpoint works (saves file before sending to front) - cache gui class instanciated for testing only

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,15 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 
 from django.views.decorators.csrf import csrf_exempt
 import os
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 from backend.settings import BASE_DIR
 import random
 
+from alg.image_api import Gui as Cache
+
+test_cache = Cache()
+
+example = "apple"
+suffix = example.upper()
+p = os.path.join("..", "sample_data", example)
+
+test_cache.setup(series_path=p, suffix=suffix, extension="jpg", height=500, width=900)
+
+
 
 def test(request):
     return HttpResponse("<html><body>Reached test!</body></html>")
-
 
 
 
@@ -21,6 +34,7 @@ def upload_images(request):
         if not file:
             raise ValueError
     # load images to directory
+
     except:
         response = HttpResponse(request.request.FILES)
         response.status_code = 400
@@ -67,16 +81,32 @@ def focus(request):
 
 def viewpoint(request):
     try:
-        shift_start = request.GET.get('shift_start')
-        shift_end = request.GET.get('shift_end')
-        move = request.GET.get('move')
-        stereo = request.GET.get('stereo')
-        path = os.path.join(BASE_DIR, 'sample_data', 'apples', 'APPLE001.jpg')
-        with open(path, "rb") as f:
+        res:np.ndarray = test_cache.get_last_result()
+        try:
+            slice = request.GET.get('slice')
+            if slice not in [None, "", "()", "((),())", (), []]:
+                print("Viewpoint - slice {}".format(slice))
+
+                res = test_cache.viewpoint(slice=slice)
+            else:
+                shift = float(request.GET.get('shift'))
+                move = float(request.GET.get('move'))
+                stereo = float(request.GET.get('stereo'))
+                print("Viewpoint - move: {} stereo: {} shift: {}".format(move, stereo, shift))
+
+                res = test_cache.viewpoint(shift=shift, move=move, stereo=stereo)
+        except Exception as e:
+            print(e)
+
+        plt.imsave("tmp.jpeg", res)
+        with open("tmp.jpeg", 'rb') as f:
             return HttpResponse(f.read(), content_type="image/jpeg")
+        # path = os.path.join(BASE_DIR, 'sample_data', 'apples', 'APPLE001.jpg')
+
 
     # load images to director
-    except:
+    except Exception as e:
+        print(e)
         response = HttpResponse('')
         response.status_code = 400
         return response
